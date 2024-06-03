e00e54878478aaf9073000699edf0dd1b6f7b537
Fernandolass <152617843+Fernandolass@users.noreply.github.com>
2024-06-02 22:40:25 -0300

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const bcrypt = require('bcryptjs');
 const path = require('path');
 const app = express();
-const port = 3000;
+const port = 3306;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
