5f7ff3fa6abc9ea6ea53c9315bb00c169bbd62ad
LuizCaiaSM <148289593+LuizCaiaSM@users.noreply.github.com>
2024-06-03 15:38:09 -0300

    Delete db.sql

--- db.sql
+++
@@ -1,8 +0,0 @@
-CREATE DATABASE loginecadastro;
-USE loginecadastro;
-
-CREATE TABLE users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    email VARCHAR(255) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL
-);
